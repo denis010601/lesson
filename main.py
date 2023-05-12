c85c818ec76290ee7d2b9bcee1d30d1483e06c22
dart161 <k_d_i01@mail.ru>
2023-05-12 21:22:47 +0400

    поигрался в main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# type_int = 1 # целые числа (int)
+# type_float = 1.23 # числа с точкой
+# type_str = "Строки пишутся в кавычках"
+# type_bool_one = True # булевы значения истина(правда)
+# type_bool_two = False # булевы значения ложь
+# type_none = None
+user_name = "Denis"
+print(user_name*2)
+print(user_name*3)
+print(user_name*4)
+print(user_name*5)
+print(user_name*6)
+print(user_name*7)
+print(user_name*8)
+print(user_name*9)
